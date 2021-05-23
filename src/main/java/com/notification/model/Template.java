2f248caf2354c578a073dc7568d146405f80c04f
Anuj Mathur <anujmathur@Anujs-MacBook-Pro.local>
2021-05-24 00:49:24 +0530

    Adding title and description to template

--- src/main/java/com/notification/model/Template.java
+++ src/main/java/com/notification/model/Template.java
@@ -6,12 +6,16 @@ import com.notification.model.channel.slack.SlackTemplate;
 
 public class Template {
     private final String id;
+    private final String title;
+    private final String description;
     private final EmailTemplate emailTemplate;
     private final SlackTemplate slackTemplate;
     private final InAppTemplate inAppTemplate;
 
-    public Template(String id, EmailTemplate emailTemplate, SlackTemplate slackTemplate, InAppTemplate inAppTemplate) {
+    public Template(String id, String title, String description, EmailTemplate emailTemplate, SlackTemplate slackTemplate, InAppTemplate inAppTemplate) {
         this.id = id;
+        this.title = title;
+        this.description = description;
         this.emailTemplate = emailTemplate;
         this.slackTemplate = slackTemplate;
         this.inAppTemplate = inAppTemplate;
