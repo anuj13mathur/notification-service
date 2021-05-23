b41faf4d2aba2f9cdf49dea4870a34565b2ad51e
Anuj Mathur <anujmathur@Anujs-MacBook-Pro.local>
2021-05-24 04:15:16 +0530

    1).Adding swagger documentation 2).Refactoring template structure

--- src/main/java/com/notification/model/template/AllChannelTemplate.java
+++ src/main/java/com/notification/model/template/AllChannelTemplate.java
@@ -1,21 +1,18 @@
-package com.notification.model;
+package com.notification.model.template;
 
 import com.notification.model.channel.email.EmailTemplate;
 import com.notification.model.channel.inapp.InAppTemplate;
 import com.notification.model.channel.slack.SlackTemplate;
 
-public class Template {
-    private final String id;
-    private final String title;
-    private final String description;
+public class AllChannelTemplate {
+    private final TemplateMetadata metadata;
     private final EmailTemplate emailTemplate;
     private final SlackTemplate slackTemplate;
     private final InAppTemplate inAppTemplate;
 
-    public Template(String id, String title, String description, EmailTemplate emailTemplate, SlackTemplate slackTemplate, InAppTemplate inAppTemplate) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
+    public AllChannelTemplate(TemplateMetadata metadata, EmailTemplate emailTemplate, SlackTemplate slackTemplate,
+                              InAppTemplate inAppTemplate) {
+        this.metadata = metadata;
         this.emailTemplate = emailTemplate;
         this.slackTemplate = slackTemplate;
         this.inAppTemplate = inAppTemplate;
---
+++ src/main/java/com/notification/model/template/TemplateMetadata.java
@@ -0,0 +1,13 @@
+package com.notification.model.template;
+
+public class TemplateMetadata {
+    private final String id;
+    private final String title;
+    private final String description;
+
+    public TemplateMetadata(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+}
